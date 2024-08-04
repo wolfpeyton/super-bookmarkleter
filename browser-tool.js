84eff8df938c892edcf1fde30eb767e0d5564b51
Peyton Wolf <peytonwolf@gmail.com>
2024-08-04 18:24:28 -0400

    add alternate output behavior when %s flag is on

--- browser-tool.js
+++ browser-tool.js
@@ -26,6 +26,8 @@ app.controller('appController', ['$scope', function ($scope) {
 
   $scope.error = null;
 
+  var previousBlobUrl = null;
+
   $scope.updateBookmarklet = function (options) {
 
     var code = $scope.input;
@@ -51,6 +53,19 @@ app.controller('appController', ['$scope', function ($scope) {
       $scope.error = err.message;
       $scope.output = null;
       return;
+    }
+
+    if (options.preserveSiteSearch) {
+      var blob = new Blob([$scope.output], { type: 'text/plain' });
+      var url = URL.createObjectURL(blob);
+      var downloadLink = document.getElementById('DownloadHref');
+      downloadLink.href = url;
+
+      // Revoke the previous Blob URL to free up memory
+      if (previousBlobUrl) {
+        URL.revokeObjectURL(previousBlobUrl);
+      }
+      previousBlobUrl = url;
     }
 
   };
